8301b7ca28880298b7b1afa44e789fc18c802198
Anton Kit <croixua@gmail.com>
2022-05-29 22:08:30 +0300

    first worked version

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import Searchbar from './Searchbar/Searchbar';
+import Gallery from './Gallery/Gallery';
+import s from './App.module.css';
+
+export default class App extends Component {
+  state = {
+    searchQuery: '',
+  };
+
+  handleFormSubmit = searchQuery => {
+    this.setState({ searchQuery });
+  };
+
+  render() {
+    const { searchQuery } = this.state;
+
+    return (
+      <div className={s.App}>
+        <Searchbar onSubmit={this.handleFormSubmit} />
+        <Gallery searchQuery={searchQuery} />
+      </div>
+    );
+  }
+}
